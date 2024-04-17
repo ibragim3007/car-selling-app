dea08c1ca05e929e7270ff7553a2628bb15319a9
Ibragim <lol.3007@mail.ru>
2024-04-17 12:52:55 -0700

    prepare typography

---
+++ src/shared/config/styles/typography/typography.ts
@@ -0,0 +1,48 @@
+import { TextProps } from "react-native";
+
+type TypographyVariants =
+  | "largeTitle"
+  | "title-1"
+  | "title-2"
+  | "title-3"
+  | "headline"
+  | "body"
+  | "callout"
+  | "subhead"
+  | "footnote"
+  | "caption-1"
+  | "caption-2";
+
+type TypographyWeight = "regular" | "bold" | "medium";
+
+export interface TypographyProps extends TextProps {
+  variant?: TypographyVariants;
+  weight?: TypographyWeight;
+}
+
+export const TypographyStyles: Record<TypographyVariants, TextProps["style"]> =
+  {
+    largeTitle: { fontSize: 34 },
+    "title-1": { fontSize: 28 },
+    "title-2": { fontSize: 22 },
+    "title-3": { fontSize: 20 },
+    headline: { fontSize: 17 },
+    body: { fontSize: 17 },
+    callout: { fontSize: 16 },
+    subhead: { fontSize: 15 },
+    footnote: { fontSize: 13 },
+    "caption-1": { fontSize: 12 },
+    "caption-2": { fontSize: 11 },
+  };
+
+export const fontsWeights: Record<TypographyWeight, TextProps["style"]> = {
+  regular: {
+    fontFamily: "Montserrat_400Regular",
+  },
+  bold: {
+    fontFamily: "Montserrat_700Bold",
+  },
+  medium: {
+    fontFamily: "Montserrat_500Medium",
+  },
+};
--- src/shared/ui/typography/Typography.tsx
+++ src/shared/ui/typography/Typography.tsx
@@ -1,46 +1,13 @@
+import {
+  TypographyProps,
+  TypographyStyles,
+  fontsWeights,
+} from "@/shared/config/styles/typography/typography";
 import React from "react";
 import { Text, TextProps } from "react-native";
 
-type TypographyVariants =
-  | "largeTitle"
-  | "title_1"
-  | "title_2"
-  | "title_3"
-  | "headline"
-  | "body"
-  | "callout";
-
-type TypographyWeight = "regular" | "bold" | "medium";
-
-interface TypographyProps extends TextProps {
-  variant?: TypographyVariants;
-  weight?: TypographyWeight;
-}
-
-const fontsWeights: Record<TypographyWeight, TextProps["style"]> = {
-  regular: {
-    fontFamily: "Montserrat_400Regular",
-  },
-  bold: {
-    fontFamily: "Montserrat_700Bold",
-  },
-  medium: {
-    fontFamily: "Montserrat_500Medium",
-  },
-};
-
 const Typography = (props: TypographyProps) => {
   const fontStyles = fontsWeights[props.weight || "medium"];
-
-  const TypographyStyles: Record<TypographyVariants, TextProps["style"]> = {
-    largeTitle: { fontSize: 34 },
-    title_1: { fontSize: 28 },
-    title_2: { fontSize: 22 },
-    title_3: { fontSize: 20 },
-    headline: { fontSize: 17 },
-    body: { fontSize: 17 },
-    callout: { fontSize: 15 },
-  };
 
   const mergedStyles = Object.assign(
     {},
