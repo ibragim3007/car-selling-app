d0f50380e9b8caa835b4cead4a7e0a6da5fedad5
Ibragim <lol.3007@mail.ru>
2024-04-21 16:16:50 -0700

    update to flashList

--- src/modules/CarList/components/CarList.tsx
+++ src/modules/CarList/components/CarList.tsx
@@ -2,8 +2,8 @@ import carObjects from '@/shared/mock/car1';
 import { ICar } from '@/shared/types';
 import Grid from '@/shared/ui/layout/Grid';
 import { normalizedSize } from '@/shared/utils/size';
+import { FlashList, ListRenderItem } from '@shopify/flash-list';
 import React from 'react';
-import { FlatList, ListRenderItemInfo } from 'react-native';
 import CarItem from './CarItem/CarItem';
 
 interface CarListProps {
@@ -12,20 +12,25 @@ interface CarListProps {
 }
 
 function CarList({ headerComponent, footerComponent }: CarListProps) {
-  const _renderItem = (data: ListRenderItemInfo<ICar>) => <CarItem key={data.item.id} car={data.item} />;
+  // const _renderItem = (info: ListRenderItemInfo<ICar>) => <CarItem key={info.item.id} car={info.item} />;
+
+  const renderItem: ListRenderItem<ICar> = ({ item }) => {
+    return <CarItem key={item.id} car={item} />;
+  };
   const lengthItem = normalizedSize(145);
   return (
     <Grid flex={1}>
-      <FlatList
+      <FlashList
+        data={carObjects}
+        renderItem={renderItem}
+        estimatedItemSize={lengthItem}
         removeClippedSubviews
-        initialNumToRender={5}
+        // initialNumToRender={5}
         // stickyHeaderIndices={[0]}
         ListHeaderComponent={headerComponent}
         ListFooterComponent={footerComponent}
-        renderItem={_renderItem}
-        data={carObjects}
         keyExtractor={item => item.id}
-        getItemLayout={(data, index) => ({ length: lengthItem, offset: lengthItem * index, index })}
+        // getItemLayout={(data, index) => ({ length: lengthItem, offset: lengthItem * index, index })}
       />
     </Grid>
   );
