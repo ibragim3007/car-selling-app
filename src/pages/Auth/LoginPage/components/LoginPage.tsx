5c148d2265f6111a5231ab27aa5976927ec76e6d
Ibragim <lol.3007@mail.ru>
2024-04-25 16:25:25 -0700

    add inputs

--- src/pages/Auth/LoginPage/components/LoginPage.tsx
+++ src/pages/Auth/LoginPage/components/LoginPage.tsx
@@ -1,20 +1,54 @@
 import FooterSupport from '@/components/FooterSupport/FooterSupport';
+import ErrorCard from '@/components/Informers/ErrorCard';
 import Button from '@/shared/ui/buttons/Button';
 import Input from '@/shared/ui/input/Input';
 import Grid from '@/shared/ui/layout/Grid';
 import PageBackground from '@/shared/ui/layout/PageBackground';
 import Typography from '@/shared/ui/typography/Typography';
 import React from 'react';
+import { FieldValues, useForm } from 'react-hook-form';
+
+export interface IFormInput {
+  login: string;
+  password: string;
+}
 
 const LoginPage = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const pressButton = (data: FieldValues) => {
+    console.log(data);
+  };
+
   return (
     <PageBackground paddingVertical={16} paddingHorizontal={16} color="primary">
       <Grid justfity="space-between" style={{ height: '100%' }} paddingBottom={30}>
         <Grid space="lg">
           <Grid space="md">
-            <Input label="Логин" />
             <Input
+              style={{ borderColor: 'red' }}
+              name="login"
+              control={control}
+              label="Логин"
+              rules={{
+                required: 'Логин обязателен',
+              }}
+            />
+            <Input
+              name="password"
               label="Пароль"
+              rules={{
+                required: 'Пароль обязателен',
+                minLength: {
+                  value: 4,
+                  message: 'Минимум 4 символа',
+                },
+              }}
+              control={control}
               secureTextEntry
               passwordRules={'required: upper; required: lower; required: digit; max-consecutive: 2; minlength: 8'}
               rightSide={
@@ -24,7 +58,9 @@ const LoginPage = () => {
               }
             />
           </Grid>
-          <Button>Войти</Button>
+          <ErrorCard text="Ошибка" />
+          <Button onPress={handleSubmit(pressButton)}>Войти</Button>
+
           <Grid marginVertical={20} space="sm" align="center" row justfity="center">
             <Typography variant="subhead">Нет аккаунта?</Typography>
             <Typography variant="subhead" weight="medium" color="success">
--- src/shared/ui/input/Input.tsx
+++ src/shared/ui/input/Input.tsx
@@ -2,6 +2,7 @@ import EyeIcon from '@/icons/linear/eye.svg';
 import { useTheme } from '@/shared/hooks/stylesHooks/useTheme';
 import { normalizedSize } from '@/shared/utils/size';
 import React, { useState } from 'react';
+import { Control, Controller, FieldValues, UseControllerProps } from 'react-hook-form';
 import { StyleSheet, TextInput, TextInputProps } from 'react-native';
 import PressableIcon from '../buttons/PressableButton';
 import Grid from '../layout/Grid';
@@ -11,9 +12,12 @@ export interface InputProps extends TextInputProps {
   label?: string;
   labelProps?: TypographyProps;
   rightSide?: React.ReactNode;
+  control: Control<FieldValues, any>;
+  name: string;
+  rules?: UseControllerProps['rules'];
 }
 
-const Input = ({ label, labelProps, secureTextEntry, rightSide, ...props }: InputProps) => {
+const Input = ({ label, name, rules, labelProps, control, secureTextEntry, rightSide, ...props }: InputProps) => {
   const { colors } = useTheme();
   const [showPassword, setShowPassword] = useState(secureTextEntry);
   const [isFocus, setIsFocus] = useState(false);
@@ -42,27 +46,47 @@ const Input = ({ label, labelProps, secureTextEntry, rightSide, ...props }: Inpu
         )}
         {rightSide && rightSide}
       </Grid>
-      <Grid
-        align="center"
-        style={{
-          borderWidth: 1,
-          borderColor: isFocus ? colors.text.primary : colors.divider,
-          borderRadius: colors.styles.borderRadius,
-        }}
-        row
-      >
-        <TextInput
-          textContentType="oneTimeCode"
-          onBlur={() => setIsFocus(false)}
-          onFocus={() => setIsFocus(true)}
-          secureTextEntry={showPassword}
-          {...props}
-          style={styles}
-        />
-        {secureTextEntry && (
-          <PressableIcon onPress={pressShowPassword} Icon={EyeIcon} style={{ padding: normalizedSize(12) }} />
+
+      <Controller
+        control={control}
+        name={name}
+        rules={rules}
+        render={({ field: { value, onBlur, onChange }, fieldState: { error } }) => (
+          <>
+            <Grid
+              align="center"
+              style={{
+                borderWidth: 1,
+                borderColor: error ? colors.accent.red : isFocus ? colors.text.primary : colors.divider,
+                borderRadius: colors.styles.borderRadius,
+              }}
+              row
+            >
+              <TextInput
+                value={value}
+                textContentType="oneTimeCode"
+                onBlur={() => {
+                  setIsFocus(false);
+                  return onBlur;
+                }}
+                onChangeText={onChange}
+                onFocus={() => setIsFocus(true)}
+                secureTextEntry={showPassword}
+                {...props}
+                style={styles}
+              />
+              {secureTextEntry && (
+                <PressableIcon onPress={pressShowPassword} Icon={EyeIcon} style={{ padding: normalizedSize(12) }} />
+              )}
+            </Grid>
+            {error && (
+              <Typography variant="caption-1" color="red">
+                {error?.message || 'Ошибка'}
+              </Typography>
+            )}
+          </>
         )}
-      </Grid>
+      />
     </Grid>
   );
 };
