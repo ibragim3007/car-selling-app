8e70cb7c0a3ad454ddc4c39290d753bee4cd69f3
Ibragim <lol.3007@mail.ru>
2024-04-26 11:41:32 -0700

    add home page images mock

--- src/modules/NewsSlider/components/newsItem/NewsItem.tsx
+++ src/modules/NewsSlider/components/newsItem/NewsItem.tsx
@@ -4,7 +4,7 @@ import Grid from '@/shared/ui/layout/Grid';
 import Typography from '@/shared/ui/typography/Typography';
 import { normalizedSize } from '@/shared/utils/size';
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, Pressable } from 'react-native';
 
 interface NewsItemProps extends CardProps {
   item: INews;
@@ -12,45 +12,47 @@ interface NewsItemProps extends CardProps {
 
 const NewsItem = ({ item, ...props }: NewsItemProps) => {
   return (
-    <Grid padding={0} style={{ height: normalizedSize(246) }}>
-      <Card
-        p={0}
-        borderRadius={12}
-        style={{
-          width: normalizedSize(265),
-          marginRight: 16,
-          shadowColor: '#00000093',
-          shadowOpacity: 0.1,
-          shadowRadius: 10,
-          shadowOffset: {
-            height: 10,
-            width: 0,
-          },
-        }}
-        {...props}
-      >
-        <Grid gap={12} padding={0}>
-          <Image
-            source={{ uri: item.coverImage }}
-            alt={item.title}
-            style={{
-              borderTopLeftRadius: 12,
-              borderTopRightRadius: 12,
-              width: normalizedSize(265),
-              height: normalizedSize(136),
-            }}
-          />
-          <Grid paddingHorizontal={12} paddingBottom={12} space="sm">
-            <Typography weight="bold" variant="footnote">
-              Нарулили правый руль
-            </Typography>
-            <Typography variant="caption-1" numberOfLines={2}>
-              Теперь можно заказать полный отчет на праворульный авто.
-            </Typography>
+    <Pressable>
+      <Grid padding={0} style={{ height: normalizedSize(246) }}>
+        <Card
+          p={0}
+          borderRadius={12}
+          style={{
+            width: normalizedSize(265),
+            marginRight: 16,
+            shadowColor: '#00000093',
+            shadowOpacity: 0.1,
+            shadowRadius: 10,
+            shadowOffset: {
+              height: 10,
+              width: 0,
+            },
+          }}
+          {...props}
+        >
+          <Grid gap={12} padding={0}>
+            <Image
+              source={{ uri: item.coverImage }}
+              alt={item.title}
+              style={{
+                borderTopLeftRadius: 12,
+                borderTopRightRadius: 12,
+                width: normalizedSize(265),
+                height: normalizedSize(136),
+              }}
+            />
+            <Grid paddingHorizontal={12} paddingBottom={12} space="sm">
+              <Typography weight="bold" numberOfLines={1} lineBreakMode="tail" variant="footnote">
+                {item.title}
+              </Typography>
+              <Typography variant="caption-1" numberOfLines={2} lineBreakMode="tail">
+                {item.shortDescription}
+              </Typography>
+            </Grid>
           </Grid>
-        </Grid>
-      </Card>
-    </Grid>
+        </Card>
+      </Grid>
+    </Pressable>
   );
 };
 
--- src/shared/mock/news1.ts
+++ src/shared/mock/news1.ts
@@ -11,11 +11,11 @@ function getRandomNumber(min: number, max: number) {
 
 // Фиктивные ссылки на изображения
 const imageUrls = [
-  'https://via.placeholder.com/400',
-  'https://via.placeholder.com/500',
-  'https://via.placeholder.com/600',
-  'https://via.placeholder.com/700',
-  'https://via.placeholder.com/800',
+  'https://images.ctfassets.net/hrltx12pl8hq/a2hkMAaruSQ8haQZ4rBL9/8ff4a6f289b9ca3f4e6474f29793a74a/nature-image-for-website.jpg?fit=fill&w=600&h=400',
+  'https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg',
+  'https://tinyjpg.com/images/social/website.jpg',
+  'https://static.vecteezy.com/system/resources/thumbnails/025/284/015/small_2x/close-up-growing-beautiful-forest-in-glass-ball-and-flying-butterflies-in-nature-outdoors-spring-season-concept-generative-ai-photo.jpg',
+  'https://images.pexels.com/photos/1266810/pexels-photo-1266810.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
 ];
 
 // Генерация мокнутых объектов INews
