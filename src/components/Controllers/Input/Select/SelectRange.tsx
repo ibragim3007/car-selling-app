2e68024d1a8ef1e3e5af07ad6d5d15917c858d67
Ibragim <lol.3007@mail.ru>
2024-05-17 15:56:55 -0700

    fix range selection

--- src/components/Controllers/Input/Select/SelectRange.tsx
+++ src/components/Controllers/Input/Select/SelectRange.tsx
@@ -1,7 +1,6 @@
 import BottomSheetModal from '@/components/Modal/BottomSheetModal';
 import QuitResetHeader from '@/components/Modal/components/QuitResetHeader';
 import AcceptButton from '@/components/ModalCheckboxList/components/AcceptButton';
-import { formatNumber } from '@/shared/helpers/formatMileage';
 import { IFilterCreate, TRange } from '@/shared/types/filters.types';
 import Grid from '@/shared/ui/layout/Grid';
 import { BottomSheetModal as BSM } from '@gorhom/bottom-sheet';
@@ -9,6 +8,7 @@ import React, { useRef, useState } from 'react';
 import { InputProps } from '../Input';
 import Range from '../Range/Range';
 import SelectButtonWrap from './SelectButtonWrap';
+import { formatNumber } from '@/shared/helpers/formatMileage';
 
 interface SelectRangeProps extends InputProps {
   title: string;
@@ -27,24 +27,28 @@ const SelectRange = ({ title, name, values, subtitleInput, onChangeValues, dataM
     buttomSheetRef.current?.present();
   };
 
-  const [firstRange, setFirstRange] = useState(firstValue);
-  const updateFirstRange = (newValue: number) => setFirstRange(newValue);
+  const [firstRange, setFirstRange] = useState(firstValue || 0);
+  const updateFirstRange = (newValue: number) => setFirstRange(newValue || 0);
 
-  const [secondRange, setSecondRange] = useState(secondValue);
-  const updateSecondRange = (newValue: number) => setSecondRange(newValue);
+  const [secondRange, setSecondRange] = useState(secondValue || 0);
+  const updateSecondRange = (newValue: number) => setSecondRange(newValue || 0);
+
+  const [inputPlaceholder, setInputPlaceholder] = useState(
+    inputPlaceholderText(firstRange, secondRange, subtitleInput),
+  );
 
   const pressAccept = () => {
-    console.log('SET: ', firstRange, secondRange);
-    if (firstRange !== undefined && secondRange !== undefined) onChangeValues([firstRange, secondRange]);
+    if (firstRange !== undefined && secondRange !== undefined) {
+      onChangeValues([firstRange, secondRange]);
+      setInputPlaceholder(inputPlaceholderText(firstRange, secondRange, subtitleInput));
+    }
     buttomSheetRef.current?.dismiss();
   };
 
-  console.log(firstRange, secondRange);
+  // const inputPlaceholder = inputPlaceholderText(firstRange, secondRange, subtitleInput);
 
-  const inputPlaceholder =
-    !firstValue || !secondValue
-      ? 'Неважно'
-      : `${formatNumber(firstValue || 0)} - ${formatNumber(secondValue || 0)} ${subtitleInput ? subtitleInput : ''}`;
+  const filteredSecondRange = dataMock?.filter(a => a > Number(firstRange || 0)) || [];
+
   return (
     <>
       <SelectButtonWrap value={inputPlaceholder} name={name} onPress={onPresent} {...props} />
@@ -52,12 +56,7 @@ const SelectRange = ({ title, name, values, subtitleInput, onChangeValues, dataM
         <Grid flex={1}>
           <Grid padding={16} row gap={1}>
             <Range label="От" onChange={updateFirstRange} currentValue={firstRange} data={dataMock} />
-            <Range
-              label="До"
-              onChange={updateSecondRange}
-              currentValue={secondRange}
-              data={dataMock?.filter(a => a > Number(firstRange || 0))}
-            />
+            <Range label="До" onChange={updateSecondRange} currentValue={secondRange} data={filteredSecondRange} />
           </Grid>
 
           <AcceptButton onPress={pressAccept} flex={1} />
@@ -66,5 +65,11 @@ const SelectRange = ({ title, name, values, subtitleInput, onChangeValues, dataM
     </>
   );
 };
+
+function inputPlaceholderText(firstValue: number, secondValue: number, subtitleInput?: string) {
+  if (firstValue === 0 || secondValue === 0) return 'Неважно';
+  else if (firstValue !== 0 && secondValue !== 0)
+    return `${formatNumber(firstValue)} – ${formatNumber(secondValue)} ${subtitleInput}`;
+}
 
 export default SelectRange;
