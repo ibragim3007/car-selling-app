174089b90f32172e8666dc9c9880e1e66ff89516
Ibragim <lol.3007@mail.ru>
2024-06-04 19:44:52 -0700

    short filter block

--- src/modules/MyFilters/components/FilterComponent/FilterComponent.tsx
+++ src/modules/MyFilters/components/FilterComponent/FilterComponent.tsx
@@ -18,11 +18,14 @@ import Grid from '@/shared/ui/layout/Grid';
 import { normalizedSize } from '@/shared/utils/size';
 import { router } from 'expo-router';
 import React, { useState } from 'react';
+import { LayoutChangeEvent, View } from 'react-native';
 import CarsDisplay from './Section/CarsDisplay';
 import SectionWrapper from './Section/SectionWrapper';
 interface FilterComponentProps {
   filter: IFilter;
 }
+
+const HEIGHT_FILTER = 500;
 
 const FilterComponent = ({ filter }: FilterComponentProps) => {
   const [deleteFilter, { isLoading }] = useDeleteFilterMutation();
@@ -30,63 +33,81 @@ const FilterComponent = ({ filter }: FilterComponentProps) => {
 
   const lastOwner = filter.ownersCount[filter.ownersCount.length - 1];
 
+  const [showButton, setShowButton] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
 
+  const onLayout = (event: LayoutChangeEvent) => {
+    const { height } = event.nativeEvent.layout;
+
+    setShowButton(height > HEIGHT_FILTER);
+  };
+
   return (
-    <CardTitle
-      style={{ overflow: 'hidden' }}
-      headerProps={{ style: { paddingVertical: normalizedSize(8), paddingHorizontal: normalizedSize(16) } }}
-      titleProps={{ style: { paddingHorizontal: 0, paddingVertical: 0 } }}
-      title={filter.name}
-      isNoPadding
-      rightHeader={
-        <Dropdown
-          isLoading={isLoading}
-          items={[
-            {
-              Icon: EditIcon,
-              title: 'Редактировать',
-              onPress: () => router.push(`(mycollections)/filters/${filter.id}`),
-            },
-            {
-              title: 'Копировать',
-            },
-            {
-              title: 'Удалить',
-              onPress: () => deleteFilter(filter.id),
-              color: 'red',
-            },
-          ]}
-        />
-      }
-    >
-      <LayoutAnimation>
-        <Grid paddingHorizontal={16} space="sm" style={{ maxHeight: isOpen ? 'auto' : 310, overflow: 'hidden' }}>
-          <Divider />
-          <Grid space="lg">
-            <TitleSwitch onChange={toggleEnableFilter} title="Включить подборку" value={filter.enabled} />
-            <TitleSwitch onChange={toggleNotifications} title="Уведомления (Telegram)" value={filter.notifications} />
+    <View onLayout={onLayout}>
+      <CardTitle
+        style={{ overflow: 'hidden' }}
+        headerProps={{ style: { paddingVertical: normalizedSize(8), paddingHorizontal: normalizedSize(16) } }}
+        titleProps={{ style: { paddingHorizontal: 0, paddingVertical: 0 } }}
+        title={filter.name}
+        isNoPadding
+        rightHeader={
+          <Dropdown
+            isLoading={isLoading}
+            items={[
+              {
+                Icon: EditIcon,
+                title: 'Редактировать',
+                onPress: () => router.push(`(mycollections)/filters/${filter.id}`),
+              },
+              {
+                title: 'Копировать',
+              },
+              {
+                title: 'Удалить',
+                onPress: () => deleteFilter(filter.id),
+                color: 'red',
+              },
+            ]}
+          />
+        }
+      >
+        <LayoutAnimation>
+          <Grid
+            paddingHorizontal={16}
+            space="sm"
+            style={{ maxHeight: isOpen ? 'auto' : HEIGHT_FILTER, overflow: 'hidden' }}
+          >
+            <Divider />
+            <Grid space="lg">
+              <TitleSwitch onChange={toggleEnableFilter} title="Включить подборку" value={filter.enabled} />
+              <TitleSwitch onChange={toggleNotifications} title="Уведомления (Telegram)" value={filter.notifications} />
+            </Grid>
+            <Divider />
+            <CarsDisplay filter={filter} />
+
+            {filter.regions && (
+              <SectionWrapper title="География">
+                <DisplayGeography regions={filter.regions} />
+              </SectionWrapper>
+            )}
+
+            <TableInfo title="Пробег, км" value={filter.mileages?.map(item => formatNumber(item)).join(' - ')} />
+            <TableInfo title="Цена, ₽" value={filter.prices?.map(item => formatNumber(item)).join(' - ')} />
+            <TableInfo title="Год выпуска" value={filter.years?.join(' - ')} />
+            <TableInfo title="Объем двигателя, л" value={filter.iceValues?.join(' - ')} />
+            <TableInfo title="Тип топлива" value={filter.ices?.map(item => enumCompare(ices, item)).join(', ')} />
+            <TableInfo title="Коробка" value={filter.gears?.map(item => enumCompare(gears, item)).join(', ')} />
+            <TableInfo title="Владельцы" value={enumCompare(owners, lastOwner)} />
           </Grid>
-          <Divider />
-          <CarsDisplay filter={filter} />
-
-          <SectionWrapper title="География">
-            <DisplayGeography regions={filter.regions || []} />
-          </SectionWrapper>
-
-          <TableInfo title="Пробег, км" value={filter.mileages?.map(item => formatNumber(item)).join(' - ')} />
-          <TableInfo title="Цена, ₽" value={filter.prices?.map(item => formatNumber(item)).join(' - ')} />
-          <TableInfo title="Год выпуска" value={filter.years?.join(' - ')} />
-          <TableInfo title="Объем двигателя, л" value={filter.iceValues?.join(' - ')} />
-          <TableInfo title="Тип топлива" value={filter.ices?.map(item => enumCompare(ices, item)).join(', ')} />
-          <TableInfo title="Коробка" value={filter.gears?.map(item => enumCompare(gears, item)).join(', ')} />
-          <TableInfo title="Владельцы" value={enumCompare(owners, lastOwner)} />
-        </Grid>
-      </LayoutAnimation>
-      <Button onPress={() => setIsOpen(!isOpen)} style={{ marginTop: 10 }} variant="text" color="black">
-        {isOpen ? 'Скрыть' : 'Раскрыть'} <ArrowDown style={{ transform: [{ rotate: isOpen ? '0deg' : '180deg' }] }} />
-      </Button>
-    </CardTitle>
+        </LayoutAnimation>
+        {showButton && (
+          <Button onPress={() => setIsOpen(!isOpen)} style={{ marginTop: 10 }} variant="text" color="black">
+            {isOpen ? 'Скрыть' : 'Раскрыть'}
+            <ArrowDown style={{ transform: [{ rotate: isOpen ? '0deg' : '180deg' }] }} />
+          </Button>
+        )}
+      </CardTitle>
+    </View>
   );
 };
 
